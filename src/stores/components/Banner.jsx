301f3a0c8f8ab5482c87f6bc9bb108720137f27c
GnanaPrakash <your-email@gmail.com>
2026-06-24 14:38:07 +0530

    full project

---
+++ src/stores/components/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div>
+      <img
+        src="https://rukminim2.flixcart.com/fk-p-flap/1600/270/image/banner.jpg"
+        width="100%"
+      />
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/stores/components/Categories.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Categories = () => {
+  return (
+    <div className="cat">
+      <p>Mobiles</p>
+      <p>Computers</p>
+      <p>Watch</p>
+      <p>Men</p>
+      <p>Women</p>
+    </div>
+  )
+}
+
+export default Categories
